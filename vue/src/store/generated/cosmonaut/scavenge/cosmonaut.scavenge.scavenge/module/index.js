6ea6387f8814be41c95880b8956284f441334207
Jelilat Anofiu <jelilat@Jelilats-MBP.lan>
2022-03-14 08:12:21 +0100

    blockchain intercation

--- vue/src/store/generated/cosmonaut/scavenge/cosmonaut.scavenge.scavenge/module/index.js
+++ vue/src/store/generated/cosmonaut/scavenge/cosmonaut.scavenge.scavenge/module/index.js
@@ -3,12 +3,12 @@ import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
 import { MsgSubmitScavenge } from "./types/scavenge/tx";
-import { MsgRevealSolution } from "./types/scavenge/tx";
 import { MsgCommitSolution } from "./types/scavenge/tx";
+import { MsgRevealSolution } from "./types/scavenge/tx";
 const types = [
     ["/cosmonaut.scavenge.scavenge.MsgSubmitScavenge", MsgSubmitScavenge],
-    ["/cosmonaut.scavenge.scavenge.MsgRevealSolution", MsgRevealSolution],
     ["/cosmonaut.scavenge.scavenge.MsgCommitSolution", MsgCommitSolution],
+    ["/cosmonaut.scavenge.scavenge.MsgRevealSolution", MsgRevealSolution],
 ];
 export const MissingWalletError = new Error("wallet is required");
 export const registry = new Registry(types);
@@ -30,8 +30,8 @@ const txClient = async (wallet, { addr: addr } = { addr: "http://localhost:26657
     return {
         signAndBroadcast: (msgs, { fee, memo } = { fee: defaultFee, memo: "" }) => client.signAndBroadcast(address, msgs, fee, memo),
         msgSubmitScavenge: (data) => ({ typeUrl: "/cosmonaut.scavenge.scavenge.MsgSubmitScavenge", value: MsgSubmitScavenge.fromPartial(data) }),
-        msgRevealSolution: (data) => ({ typeUrl: "/cosmonaut.scavenge.scavenge.MsgRevealSolution", value: MsgRevealSolution.fromPartial(data) }),
         msgCommitSolution: (data) => ({ typeUrl: "/cosmonaut.scavenge.scavenge.MsgCommitSolution", value: MsgCommitSolution.fromPartial(data) }),
+        msgRevealSolution: (data) => ({ typeUrl: "/cosmonaut.scavenge.scavenge.MsgRevealSolution", value: MsgRevealSolution.fromPartial(data) }),
     };
 };
 const queryClient = async ({ addr: addr } = { addr: "http://localhost:1317" }) => {
